ad1dde481062db031f0cbaa4bf57f31b6a99d045
Ryan Voots <ryan@voots.org>
2016-12-31 15:57:02 -0500

    More work

--- lib/App/Spamfilter.pm
+++ lib/App/Spamfilter.pm
@@ -17,6 +17,10 @@ sub is_spam {
         return 1 unless ($blogspam->test_comment());
     }
 
+    if ($cfg->{features}{akismet}) {
+        # TODO use Net::Akismet
+    }
+
     return 2 if ($who =~ /^[A-Z]\w+\s+[A-Z]\w+$/); # block proper names, probably spam
     return 3 if ($what =~ m|https?://|); # no links in the desc, maybe relax later
     return 0; # we thought it wasn't spam
