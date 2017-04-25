20ed30d7b3a030525daa98d25d6ada051e18c772
Ryan Voots <ryan@voots.org>
2017-04-24 20:13:29 -0400

    Make this a model, soon to get channels and langs from perlbot

--- lib/App/Model/Perlbot.pm
+++ lib/App/Model/Perlbot.pm
@@ -1,21 +1,19 @@
-package IRC::Perlbot;
+package App::Model::Perlbot;
 
 use strict;
 use warnings;
 use v5.22;
 use Data::Dumper;
 
-#use Exporter qw/import/;
-#our @EXPORT=qw/get_eval/;
+use Mojo::Base '-base';
 
-use App::Config;
+has config => App::Config::get_config('announce');
 
 sub announce {
+    my $self = shift;
     my ($channel, $who, $what, $link) = @_;
 
-    print Dumper($cfg->{announce});
-
-    my $socket = IO::Socket::INET->new(  PeerAddr => $cfg->{announce}{server} //'localhost', PeerPort => $cfg->{announce}{port} //1784 )
+    my $socket = IO::Socket::INET->new(  PeerAddr => $self->config->{server} //'localhost', PeerPort => $self->cfg->{port} //1784 )
         or die "error: cannot connect to announce server";
 
     print $socket "$channel\x1E$link\x1E$who\x1E$what\n";
