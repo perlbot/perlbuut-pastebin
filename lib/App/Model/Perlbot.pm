ecbb38029e3cb16001108c52d01d98da8f6bbc30
Ryan Voots <ryan@voots.org>
2018-05-22 03:25:43 -0400

    Fix to new setup

--- lib/App/Model/Perlbot.pm
+++ lib/App/Model/Perlbot.pm
@@ -13,8 +13,8 @@ sub announce {
     my $self = shift;
     my ($channel, $who, $what, $link) = @_;
 
-    my $socket = IO::Socket::INET->new(  PeerAddr => $self->config->{server} //'localhost', PeerPort => $self->config->{port} //1784 )
-        or die "error: cannot connect to announce server";
+    my $socket = IO::Socket::INET->new(  PeerAddr => $self->config->{host}, PeerPort => $self->config->{port} )
+        or die "error: cannot connect to announce server: $! ".$self->config->{host} . ":" .$self->config->{port};
 
     print $socket "$channel\x1E$link\x1E$who\x1E$what\n";
     close($socket);
